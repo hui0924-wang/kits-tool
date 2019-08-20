fbcfa441e9e4ff45a1d5a9ddf24294081151c46c
琼 <1473078955@qq.com>
2019-08-20 22:57:49 +0800

    例子
    列举例子表示获取方式

--- kits.js
+++ kits.js
@@ -28,19 +28,44 @@ kits.formatDate = function () {
   return year + '-' + month + '-' + day + ' ' + hour + ':' + minute + ':' + second;
 }
 
-kits.randomInt = function(n,m){
-  return Math.floor(Math.random() * (m-n+1) + n);
+kits.randomInt = function (n, m) {
+  return Math.floor(Math.random() * (m - n + 1) + n);
 }
 
 // 常见的给id的方式1
 // 当前时间戳 + 大的随机数
-kits.getId = function(){
+kits.getId = function () {
   // 返回一个不容易重复的id
   let date = new Date();
   let time = date.getTime();// 得到的是从1970年1月1日到现在为止的毫秒总数
   // 然后在得到一个足够大的随机数，把毫秒和随机数相连，作为新的id
-  let r = this.randomInt(100000,999999);
+  let r = this.randomInt(100000, 999999);
   // 把两个数字连起来
   let id = time + '' + r;
   return id;
 }
+// 获取一个随机的16进制的颜色
+kits.randomHexColor = function () {
+  let arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+  // 随机索引(0,15);
+  let randomIndex = this.randomInt(0, 15);
+  let hex = '';
+  for (let i = 0; i < 6; i++) {
+    hex += arr[randomIndex];
+    randomIndex = this.randomInt(0, 15);
+  }
+  let hexColor = '#' + hex
+  return hexColor;
+}
+
+
+// 获取一个随机的rgb格式的颜色
+kits.randomRGBColor = function () {
+  // 颜色是在0到255之间的随机整数；
+  let r = this.randomInt(0, 255);
+  let g = this.randomInt(0, 255);
+  let b = this.randomInt(0, 255);
+  let rgbColor = 'rgb(' + r + ',' + g + ',' + b + ')';
+  return rgbColor;
+}
+
